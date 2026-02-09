c85aa7d8e126493f47a0f765782e6fbf930e09c8
Seb GG <sebastien.giguet@ynov.com>
2026-02-09 15:52:55 +0100

    test

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,9 +1,17 @@
+import { setRequestLocale } from 'next-intl/server';
 import Hero from '@/components/Hero';
 import Stats from '@/components/Stats';
 import Features from '@/components/Features';
 import PriceChart from '@/components/PriceChart';
 
-export default function HomePage() {
+export default async function HomePage({
+  params
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
+  setRequestLocale(locale);
+
   return (
     <div>
       <Hero />
